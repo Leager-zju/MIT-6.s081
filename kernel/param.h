fdc47840875a1b45fba8d9ee3c8a2725d647a910
Leager-zju <1004729740@qq.com>
2023-10-22 10:04:14 +0000

    ready to submit my lab

--- kernel/param.h
+++ kernel/param.h
@@ -11,3 +11,7 @@
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
 #define FSSIZE       1000  // size of file system in blocks
 #define MAXPATH      128   // maximum file path name
+
+#define STDIN 0     // file description of stdin
+#define STDOUT 1    // file description of stdout
+#define STDERR 2    // file description of stderr
