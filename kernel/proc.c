199bb31d3da3880c47bc6343b2acf2e64df9b320
Leager-zju <1004729740@qq.com>
2023-10-27 10:16:09 +0000

    update

--- kernel/proc.c
+++ kernel/proc.c
@@ -156,6 +156,11 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+  
+  if (p->usyscall)
+    kfree((void*)p->usyscall);
+  p->usyscall = 0;
+
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
